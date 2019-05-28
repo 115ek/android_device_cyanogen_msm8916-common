035d172994f9baf0051fd7f5191d7f6777eed43e
Felix <google@ix5.org>
2019-05-28 14:05:51 +0200

    msm8916-common: sepolicy: Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated.
    
    Change-Id: I046282b2a2e8c541726fb29cb0044503322d4be9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -130,7 +130,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 
 # SELinux
 include device/qcom/sepolicy-legacy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += \
+BOARD_VENDOR_SEPOLICY_DIRS += \
     $(PLATFORM_PATH)/sepolicy
 
 # Shims
